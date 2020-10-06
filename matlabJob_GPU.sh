4c51fa54a781b1170b42c1b44a2cff82771a0b55
Saurav Sthapit <wmsbzd@login1.tinis.cluster>
2020-10-06 02:08:01 +0100

    gpu

---
+++ matlabJob_GPU.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem-per-cpu=7764
+#SBATCH --gres=gpu:K80:1
+
+#SBATCH --time=48:00:00
+
+module purge
+
+module load CUDA
+module load MATLAB/2019a
+
+srun matlab -nodisplay -nosplash < train_dqn.m
